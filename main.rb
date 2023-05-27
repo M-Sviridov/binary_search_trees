cf728152d32b3c73c5aff77d38e61db78fa7d8cb
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-28 09:36:57 +1000

    Add tests for inserting values

--- main.rb
+++ main.rb
@@ -2,6 +2,16 @@
 
 require './tree'
 
-test_array = [1, 23, 12, 3, 4, 11, 5, 12, 24, 10, 100, 32, 424, 21]
+test_array = [23, 12, 3, 4, 11, 5, 12, 24, 10, 100, 32, 424, 21]
 tree = Tree.new(test_array)
+puts 'Before inserting 2:'
+tree.pretty_print
+tree.insert(2)
+puts 'After inserting 2:'
+tree.pretty_print
+tree.insert(13)
+puts 'After inserting 13:'
+tree.pretty_print
+tree.insert(50)
+puts 'After inserting 50:'
 tree.pretty_print
