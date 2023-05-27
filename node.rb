24ddaac43ceeb3fb21cebf812e888d7a8363569c
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-28 08:09:04 +1000

    Create node class and add comparable module with <=> method

---
+++ node.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# class representing a node in a binary search tree data structure
+class Node
+  include Comparable
+
+  attr_accessor :data, :left, :right
+
+  def initialize(data = nil, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+
+  def <=>(other)
+    data <=> other.data
+  end
+end
