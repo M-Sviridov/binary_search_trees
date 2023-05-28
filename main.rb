6e55c3ab365625f9ee303e716663ac7dbc0fec6e
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-28 10:05:11 +1000

    Add tests for deleting nodes with no children

--- main.rb
+++ main.rb
@@ -15,3 +15,15 @@ tree.pretty_print
 tree.insert(50)
 puts 'After inserting 50:'
 tree.pretty_print
+tree.delete(2)
+puts 'After deleting 2:'
+tree.pretty_print
+tree.delete(4)
+puts 'After deleting 4:'
+tree.pretty_print
+tree.delete(11)
+puts 'After deleting 11:'
+tree.pretty_print
+tree.delete(424)
+puts 'After deleting 424:'
+tree.pretty_print
